10db2fef6487f7de928f0d6714cb62693ebbd052
Rui Batista <ruiandrebatista@gmail.com>
2015-11-29 18:01:56 +0000

    Add org keybindings.

--- init.el
+++ init.el
@@ -109,6 +109,13 @@ exec-path-from-shell
 (setq-default TeX-master nil)
 
 
+;; Org
+     (global-set-key "\C-cl" 'org-store-link)
+     (global-set-key "\C-ca" 'org-agenda)
+     (global-set-key "\C-cc" 'org-capture)
+     (global-set-key "\C-cb" 'org-iswitchb)
+
+
 ;; Mac keyboard
 (when (string-equal system-type "darwin")
   (setq default-input-method "MacOSX")
