ffae246d2c2e083ef310d0ec5ee7a9b177745f29
Rui Batista <ruiandrebatista@gmail.com>
2016-06-18 12:25:23 +0100

    Changes.

--- init.el
+++ init.el
@@ -51,12 +51,12 @@ markdown-mode
 dropbox
 ;; Git
 magit
-;; auto completion
-auto-complete ac-nrepl company
 ;; fix itpath
 exec-path-from-shell
 ;; projects
 projectile
+;; oarentesis
+smartparens
 ))
 
 (when (not package-archive-contents)
@@ -140,6 +140,33 @@ projectile
        (global-magit-file-mode)
   :bind (("C-x g" . magit-status)))
 
+;; parentesis
+(use-package smartparens
+  :diminish smartparens-mode
+  :commands
+  smartparens-strict-mode
+  smartparens-mode
+  sp-restrict-to-pairs-interactive
+  sp-local-pair
+  :init
+  (setq sp-interactive-dwim t)
+  :config
+  (require 'smartparens-config)
+  (sp-use-smartparens-bindings)
+
+  (sp-pair "(" ")" :wrap "C-(") ;; how do people live without this?
+  (sp-pair "[" "]" :wrap "s-[") ;; C-[ sends ESC
+  (sp-pair "{" "}" :wrap "C-{")
+
+  ;; WORKAROUND https://github.com/Fuco1/smartparens/issues/543
+  (bind-key "C-<left>" nil smartparens-mode-map)
+  (bind-key "C-<right>" nil smartparens-mode-map)
+
+  (bind-key "s-<delete>" 'sp-kill-sexp smartparens-mode-map)
+  (bind-key "s-<backspace>" 'sp-backward-kill-sexp smartparens-mode-map)
+  (sp-local-pair 'scala-mode "(" nil :post-handlers '(("||\n[i]" "RET")))
+(sp-local-pair 'scala-mode "{" nil :post-handlers '(("||\n[i]" "RET") ("| " "SPC"))))
+
 ;; Mac keyboard
 (when (string-equal system-type "darwin")
   (setq default-input-method "MacOSX")
@@ -157,7 +184,7 @@ projectile
  '(markdown-command "pandoc")
  '(package-selected-packages
    (quote
-    (ws-butler web-beautify simple-httpd scala-mode python-mode paredit markdown-mode magit js2-mode jedi helm-gtags go-autocomplete git-rebase-mode git-commit-mode ggtags function-args flycheck exec-path-from-shell evernote-mode ensime dtrt-indent dropbox company-go company-ghc company-auctex clojure-test-mode clean-aindent-mode ac-nrepl))))
+    (ws-butler web-beautify simple-httpd scala-mode python-mode paredit markdown-mode magit js2-mode jedi helm-gtags go-autocomplete git-rebase-mode git-commit-mode ggtags function-args flycheck exec-path-from-shell evernote-mode ensime dtrt-indent dropbox company-go company-ghc company-auctex clojure-test-mode clean-aindent-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
